1038ac254cd57ac4e07c03801f8a8a84b4bac429
Bryan <bryansyng2@gmail.com>
2017-08-11 18:46:25 -0600

    Construccion de metodos metodo constructor matriz() metodo construirmatriz()

--- src/practica1_201503723/Matriz.java
+++ src/practica1_201503723/Matriz.java
@@ -10,7 +10,31 @@ package practica1_201503723;
  * @author Bryan
  */
 public class Matriz {
-    
+    String [][] matrizminas;
+    String [][] matrizjuego;
+    int fila, columna, mina;
+    public void Matriz(int nivel){
+        if(nivel==1){
+            fila = 4;
+            columna = 4;
+            mina=4;
+        }
+        else if(nivel==2){
+            fila =6;
+            columna=6;
+            mina=8;
+        }
+        else if(nivel ==3){
+            fila = 8;
+            columna = 8;
+            mina = 12;
+        }
+        
+    }
+    public void ConstruirMatriz(int fila, int columna){
+         matrizminas=new String[fila][columna];
+         matrizjuego=new String[fila][columna];
+        }
     
     
 }
